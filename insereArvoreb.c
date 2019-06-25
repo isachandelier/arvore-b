708030ee5e82bf08556a05c72ec4d0c14f3b5411
isachandelier <52184546+isachandelier@users.noreply.github.com>
2019-06-24 22:33:50 -0300

    Add files via upload

---
+++ insereArvoreb.c
@@ -0,0 +1,92 @@
+#include "arvoreb.h"
+
+/*Função que divide y em dois de modo que x, após ser ajustado, ganhe um filho*/
+Arvore* dividir_no (Arvore *x, int i, Arvore *y) {
+    /*Criação de um novo nó que armazenará t-1 chaves de y*/
+    Arvore* z=criar();
+    z->folha=y->folha;
+    z->n=T-1;
+    /*Cópia das últimas t-1 chaves de y para z*/
+    for(int j=1;j<=T-1;j++){
+        z->chaves[j]=y->chaves[j+T];
+    }
+    /*Faz a cópia dos últimos t filhos de y para z*/
+    if(y->folha == 0){
+        for(int j=0;j<T;j++){
+            z->filhos[j]=y->filhos[j+T];
+        }
+    }
+    /*Diminui o número de chaves em y*/
+    y->n=T-1;
+    /*Cria espaço para o novo filho de x*/
+    for(int j=x->n;j>=i+1;j--){
+        x->filhos[j+1]=x->filhos[j];
+    }
+    /*Relaciona o nó x ao seu novo filho z*/
+    x->filhos[i+1]=z;
+    /*Acha a chave média de y e move todas as chaves maiores que ela um espaço para frente*/
+    for(int j=x->n;j>=i;j--){
+        x->chaves[j+1]=x->chaves[j];
+    }
+    /*Copia a chave média de y*/
+    x->chaves[i]=x->chaves[T-1];
+    /*Incrementa o número de chaves de x*/
+    x->n=x->n+1;
+
+    return x;
+}
+
+/*A função insere uma nova chave em um nó se o nó não estiver cheio*/
+Arvore* inserir_arvore_nao_cheia (Arvore *x, TIPO k) {
+    int i=x->n-1;
+    /*Checa se o nó é um nó folha*/
+    if(x->folha==TRUE){
+        /*Localiza onde a nova chave deve ser inserida e move todas as chaves maiores um espaço para frente*/
+        while(i>=0 && x->chaves[i]>k){
+            x->chaves[i+1]=x->chaves[i];
+            i--;
+        }
+        /*Move a chave para a nova localização*/
+        x->chaves[i+1]=k;
+        x->n=x->n+1;
+    }
+    else{/*Se o nó não for uma folha*/
+        /*Localiza o filho que receberá a nova chave*/
+        while(i>=0 && x->chaves[i]>k){
+            i--;
+        }
+        i++;
+        /*Checa se o filho achado está cheio*/
+        if(x->filhos[i]->n==(2*T-1)){
+            /*Se o filho estiver cheio divide esse filho em dois não-cheios*/
+            dividir_no(x,i,x->filhos[i]);
+            /*Depois da divisão, testa qual das duas metades de x receberá a nova chave*/
+            if(x->chaves[i+1]<k){
+                i++;
+            }
+        }
+        /*Chama a função recursivamente, para inserir k no filho correto*/
+        inserir_arvore_nao_cheia(x,k);
+    }
+    return x;
+}
+
+/*Função para inserir uma chave em uma árvore B:*/
+Arvore *inserir (Arvore *raiz, TIPO chave) {
+    Arvore *r = raiz;
+    if (r->n == (2*T - 1)) {
+      Arvore *s = criar();
+      s->folha = FALSE;
+      s->filhos[0] = r;
+      s = dividir_no (s, 0, r);
+      s = inserir_arvore_nao_cheia (s, chave);
+      return s;
+    }
+    else{
+      return inserir_arvore_nao_cheia (r, chave);
+    }
+}
+
+
+
+
---
+++ removeArvoreb.c
@@ -0,0 +1,110 @@
+#include "arvoreb.h"
+
+/*Caso 1 - A função remove a chave que tem o índice "index", que é uma folha, da árvore*/
+Arvore* remover_de_folha (Arvore *a, int index){
+    /*Move todas as chaves maiores que index um espaço para trás*/
+    for(int i=index+1;i<a->n;i++){
+        a->chaves[i-1]=a->chaves[i];
+    }
+    /*Diminui o número de chaves*/
+    a->n--;
+
+    return a;
+}
+
+
+/*A função remove a chave que tem o índice "index", que não é uma folha, da árvore*/
+Arvore* remover_de_nao_folha (Arvore *a, int index){
+    TIPO k = a->chaves[index];
+    TIPO predecessor, sucessor;
+
+    /*Caso 2A - Se o filho que precede k tem, ao menos, T chaves, acha o predecessor de k*/
+    if (a->filhos[index]->n >= T){
+        Arvore* anterior=a->filhos[index];
+        while(anterior->folha!=TRUE){
+            anterior=anterior->filhos[anterior->n];
+        }
+        predecessor=anterior->chaves[anterior->n-1];
+        a->chaves[index]=predecessor;
+        anterior->n--;
+    }
+    /*Caso 2B - Se o filho de a tem menos de T chaves, */
+    else if (a->filhos[index+1]->n >= T){
+        sucessor=a->filhos[index]->chaves[0];
+        a->chaves[index]=sucessor;
+        remover(a->filhos[index],sucessor);
+    }
+    /*Descrição ...*/
+    else{
+
+    }
+
+    return a;
+}
+
+
+//Função para verificar se raiz ficou vazia
+Arvore *verificar_raiz_vazia (Arvore *raiz){
+    /*Se após a remoção a raiz tiver 0 chaves, tornar o primeiro filho a nova raiz se existir filho; caso contrário ajustar a raiz para NULL. Liberar a raiz antiga*/
+
+    /*Completar!!!! */
+    printf("Completar\n");
+
+    return raiz;
+}
+
+
+
+/*Função que retorna o index da primeira chave maior ou igual à chave*/
+int buscar_index_remocao (Arvore *a, TIPO chave) {
+
+    int i = 0;
+
+    /*Procurando a chave no vetor de chaves */
+    while ((i < a->n) && (chave > a->chaves[i])) {
+     i = i + 1;
+    }
+
+    return i;
+}
+
+/*Descrição: ????*/
+Arvore *remover (Arvore *a, TIPO k){
+    int index;
+
+    /*Completar!!!!!!!!!!!!!!*/
+    if (a == NULL) {
+      /*Completar!!!*/
+      printf("Completar\n");
+    }
+
+    index = buscar_index_remocao (a, k);
+
+
+    //A chave a ser removida está presente neste nó
+    if ( index >= 0 && a->chaves[index] == k){
+      if (a->folha){
+         a = remover_de_folha (a, index);
+      }
+      else{
+         a = remover_de_nao_folha (a, index);
+      }
+    }
+    else{
+      //Se este nó é um nó folha, então a chave não está na árvore
+      if (a->folha){
+     printf("\nA chave %c não está na árvore.\n",k);
+     //printf("\nA chave %d não está na árvore.\n",k);
+         return a;
+      }
+
+      /*Completar!!!*/
+      printf("Completar\n");
+
+
+    }
+    a = verificar_raiz_vazia(a);
+
+    return a;
+}
+
